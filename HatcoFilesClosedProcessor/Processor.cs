93290f3aef71a38ce7b00155baa3559948e56fb7
Eric Busch <buscheric97@gmail.com>
2019-12-18 13:54:23 -0600

    Created code to figure out relevant columns

--- HatcoFilesClosedProcessor/Processor.cs
+++ HatcoFilesClosedProcessor/Processor.cs
@@ -54,10 +54,164 @@ namespace HatcoFilesClosedProcessor
                     Console.WriteLine("Worksheet 2: " + rangeCount["colCount2MLS"] + "x" + rangeCount["rowCount2MLS"]);
                     Console.WriteLine("Worksheet 3: " + rangeCount["colCount3MLS"] + "x" + rangeCount["rowCount3MLS"]);
 
-                    ///TO-DO: Write code to find relevant columns for all 3 worksheets
+                    // relevant columns indeces for sheet 1
+                    relevantCols.Add("MLSSellAgentCol1", 0);
+                    relevantCols.Add("MLSSellOfficeCol1", 0);
+                    relevantCols.Add("MLSListAgentCol1", 0);
+                    relevantCols.Add("MLSListOfficeCol1", 0);
+                    relevantCols.Add("MLSOwnerCol1", 0);
+                    relevantCols.Add("MLSCityCol1", 0);
+                    relevantCols.Add("MLSAddressCol1", 0);
+                    relevantCols.Add("MLSCloseDateCol1", 0);
+                    relevantCols.Add("MLSPriceCol1", 0);
+                    relevantCols.Add("MLSGFCol1", 0);
+                    relevantCols.Add("MLSEscrowCol1", 0);
+                    // relevant columns indeces for sheet 2
+                    relevantCols.Add("MLSAgentCol2", 0);
+                    relevantCols.Add("MLSOfficeCol2", 0);
+                    relevantCols.Add("MLSOtherAgentCol2", 0);
+                    relevantCols.Add("MLSOtherOfficeCol2", 0);
+                    relevantCols.Add("MLSOwnerCol2", 0);
+                    relevantCols.Add("MLSCityCol2", 0);
+                    relevantCols.Add("MLSAddressCol2", 0);
+                    relevantCols.Add("MLSCloseDateCol2", 0);
+                    relevantCols.Add("MLSPriceCol2", 0);
+                    relevantCols.Add("MLSGFCol2", 0);
+                    relevantCols.Add("MLSEscrowCol2", 0);
+                    relevantCols.Add("MLSAsSACol2", 0);
+                    relevantCols.Add("MLSClosingsCol2", 0);
+                    relevantCols.Add("MLSTCCloseCol2", 0);
+                    relevantCols.Add("MLSBSClosingCol2", 0);
+                    relevantCols.Add("MLSBSTCCloseCol2", 0);
+                    // relevant columns indeces for sheet 3
+                    relevantCols.Add("MLSAgentCol3", 0);
+                    relevantCols.Add("MLSOfficeCol3", 0);
+                    relevantCols.Add("MLSOwnerCol3", 0);
+                    relevantCols.Add("MLSCityCol3", 0);
+                    relevantCols.Add("MLSAddressCol3", 0);
+                    relevantCols.Add("MLSCloseDateCol3", 0);
+                    relevantCols.Add("MLSPriceCol3", 0);
+                    relevantCols.Add("MLSGFCol3", 0);
+                    relevantCols.Add("MLSEscrowCol3", 0);
+                    relevantCols.Add("MLSAsSACol3", 0);
+                    relevantCols.Add("MLSClosingsCol3", 0);
+                    relevantCols.Add("MLSTCCloseCol3", 0);
+                    relevantCols.Add("MLSBSClosingCol3", 0);
+                    relevantCols.Add("MLSBSTCCloseCol3", 0);
+
+                    // determine the columns in MLS file sheet 1 that have relevant information
+                    for (int i = 1; i <= rangeCount["colCount1MLS"]; i++)
+                    {
+                        if (xlRange1MLS.Cells[1, i].Value2 != null) // check that the cell is not empty
+                        {
+                            if (xlRange1MLS.Cells[1, i].Value2.ToString().Contains("Selling"))
+                                relevantCols["MLSSellAgentCol1"] = i;
+                            else if (xlRange1MLS.Cells[1, i].Value2.ToString().Contains("SA "))
+                                relevantCols["MLSSellOfficeCol1"] = i;
+                            else if (xlRange1MLS.Cells[1, i].Value2.ToString().Contains("Listing"))
+                                relevantCols["MLSListAgentCol1"] = i;
+                            else if (xlRange1MLS.Cells[1, i].Value2.ToString().Contains("LA "))
+                                relevantCols["MLSListOfficeCol1"] = i;
+                            else if (xlRange1MLS.Cells[1, i].Value2.ToString().Contains("Owner"))
+                                relevantCols["MLSOwnerCol1"] = i;
+                            else if (xlRange1MLS.Cells[1, i].Value2.ToString().Contains("City"))
+                                relevantCols["MLSCityCol1"] = i;
+                            else if (xlRange1MLS.Cells[1, i].Value2.ToString().Contains("Address"))
+                                relevantCols["MLSAddressCol1"] = i;
+                            else if (xlRange1MLS.Cells[1, i].Value2.ToString().Contains("Close Date"))
+                                relevantCols["MLSCloseDateCol1"] = i;
+                            else if (xlRange1MLS.Cells[1, i].Value2.ToString().Contains("Price"))
+                                relevantCols["MLSPriceCol1"] = i;
+                            else if (xlRange1MLS.Cells[1, i].Value2.ToString().Contains("GF"))
+                                relevantCols["MLSGFCol1"] = i;
+                            else if (xlRange1MLS.Cells[1, i].Value2.ToString().Contains("Escrow"))
+                                relevantCols["MLSEscrowCol1"] = i;
+                        }
+                    }
+                    // determine the columns in MLS file sheet 1 that have relevant information
+                    for (int i = 1; i <= rangeCount["colCount2MLS"]; i++)
+                    {
+                        if (xlRange2MLS.Cells[1, i].Value2 != null) // check that the cell is not empty
+                        {
+                            if (xlRange2MLS.Cells[1, i].Value2.ToString().Contains("Agent Name") &&
+                                !xlRange2MLS.Cells[1, i].Value2.ToString().Contains("Other"))
+                                relevantCols["MLSAgentCol2"] = i;
+                            else if (xlRange2MLS.Cells[1, i].Value2.ToString().Contains("Agent Office") &&
+                                !xlRange2MLS.Cells[1, i].Value2.ToString().Contains("Other"))
+                                relevantCols["MLSOfficeCol2"] = i;
+                            else if (xlRange2MLS.Cells[1, i].Value2.ToString().Contains("Other Agent Name"))
+                                relevantCols["MLSOtherAgentCol2"] = i;
+                            else if (xlRange2MLS.Cells[1, i].Value2.ToString().Contains("Other Agent Office"))
+                                relevantCols["MLSOtherOfficeCol2"] = i;
+                            else if (xlRange2MLS.Cells[1, i].Value2.ToString().Contains("Owner"))
+                                relevantCols["MLSOwnerCol2"] = i;
+                            else if (xlRange2MLS.Cells[1, i].Value2.ToString().Contains("City"))
+                                relevantCols["MLSCityCol2"] = i;
+                            else if (xlRange2MLS.Cells[1, i].Value2.ToString().Contains("Address"))
+                                relevantCols["MLSAddressCol2"] = i;
+                            else if (xlRange2MLS.Cells[1, i].Value2.ToString().Contains("Close Date"))
+                                relevantCols["MLSCloseDateCol2"] = i;
+                            else if (xlRange2MLS.Cells[1, i].Value2.ToString().Contains("Price"))
+                                relevantCols["MLSPriceCol2"] = i;
+                            else if (xlRange2MLS.Cells[1, i].Value2.ToString().Contains("GF"))
+                                relevantCols["MLSGFCol2"] = i;
+                            else if (xlRange2MLS.Cells[1, i].Value2.ToString().Contains("Escrow"))
+                                relevantCols["MLSEscrowCol2"] = i;
+                            else if (xlRange2MLS.Cells[1, i].Value2.ToString().Contains("As SA"))
+                                relevantCols["MLSAsSACol2"] = i;
+                            else if (xlRange2MLS.Cells[1, i].Value2.ToString().Contains("Closings") &&
+                                !xlRange2MLS.Cells[1, i].Value2.ToString().Contains("BS"))
+                                relevantCols["MLSClosingsCol2"] = i;
+                            else if (xlRange2MLS.Cells[1, i].Value2.ToString().Contains("TC Close") &&
+                                !xlRange2MLS.Cells[1, i].Value2.ToString().Contains("BS"))
+                                relevantCols["MLSTCCloseCol2"] = i;
+                            else if (xlRange2MLS.Cells[1, i].Value2.ToString().Contains("BS Closings"))
+                                relevantCols["MLSBSClosingCol2"] = i;
+                            else if (xlRange2MLS.Cells[1, i].Value2.ToString().Contains("BS TC Close"))
+                                relevantCols["MLSBSTCCloseCol2"] = i;
+                        }
+                    }
+                    // determine the columns in MLS file sheet 1 that have relevant information
+                    for (int i = 1; i <= rangeCount["colCount3MLS"]; i++)
+                    {
+                        if (xlRange3MLS.Cells[1, i].Value2 != null) // check that the cell is not empty
+                        {
+                            if (xlRange3MLS.Cells[1, i].Value2.ToString().Contains("Agent Name"))
+                                relevantCols["MLSAgentCol3"] = i;
+                            else if (xlRange3MLS.Cells[1, i].Value2.ToString().Contains("Agent Office"))
+                                relevantCols["MLSOfficeCol3"] = i;
+                            else if (xlRange3MLS.Cells[1, i].Value2.ToString().Contains("Owner"))
+                                relevantCols["MLSOwnerCol3"] = i;
+                            else if (xlRange3MLS.Cells[1, i].Value2.ToString().Contains("City"))
+                                relevantCols["MLSCityCol3"] = i;
+                            else if (xlRange3MLS.Cells[1, i].Value2.ToString().Contains("Address"))
+                                relevantCols["MLSAddressCol3"] = i;
+                            else if (xlRange3MLS.Cells[1, i].Value2.ToString().Contains("Close Date"))
+                                relevantCols["MLSCloseDateCol3"] = i;
+                            else if (xlRange3MLS.Cells[1, i].Value2.ToString().Contains("Price"))
+                                relevantCols["MLSPriceCol3"] = i;
+                            else if (xlRange3MLS.Cells[1, i].Value2.ToString().Contains("GF"))
+                                relevantCols["MLSGFCol3"] = i;
+                            else if (xlRange3MLS.Cells[1, i].Value2.ToString().Contains("Escrow"))
+                                relevantCols["MLSEscrowCol3"] = i;
+                            else if (xlRange3MLS.Cells[1, i].Value2.ToString().Contains("As SA"))
+                                relevantCols["MLSAsSACol3"] = i;
+                            else if (xlRange3MLS.Cells[1, i].Value2.ToString().Contains("Closings") &&
+                                !xlRange2MLS.Cells[1, i].Value2.ToString().Contains("BS"))
+                                relevantCols["MLSClosingsCol3"] = i;
+                            else if (xlRange3MLS.Cells[1, i].Value2.ToString().Contains("TC Close") &&
+                                !xlRange2MLS.Cells[1, i].Value2.ToString().Contains("BS"))
+                                relevantCols["MLSTCCloseCol3"] = i;
+                            else if (xlRange3MLS.Cells[1, i].Value2.ToString().Contains("BS Closings"))
+                                relevantCols["MLSBSClosingCol3"] = i;
+                            else if (xlRange3MLS.Cells[1, i].Value2.ToString().Contains("BS TC Close"))
+                                relevantCols["MLSBSTCCloseCol3"] = i;
+                        }
+                    }
 
                     ProcessorWork proc = new ProcessorWork();
-                    proc.processorWork(xlWorksheet1MLS, xlWorksheet2MLS, xlWorksheet3MLS, rangeCount, relevantCols);
+                    proc.processorWork(xlWorksheet1MLS, xlWorksheet2MLS, xlWorksheet3MLS,
+                        xlRange1MLS, xlRange2MLS, xlRange3MLS, rangeCount, relevantCols);
                 }
                 catch (Exception ex) // if an exception is caught, close the excel files so they aren't held hostage
                 {
--- HatcoFilesClosedProcessor/ProcessorWork.cs
+++ HatcoFilesClosedProcessor/ProcessorWork.cs
@@ -13,9 +13,54 @@ namespace HatcoFilesClosedProcessor
     class ProcessorWork
     {
         public void processorWork(Excel._Worksheet xlWorksheet1MLS, Excel._Worksheet xlWorksheet2MLS,
-            Excel._Worksheet xlWorksheet3MLS, Dictionary<string, int> rangeCount, Dictionary<string, int> relevantCols)
+            Excel._Worksheet xlWorksheet3MLS, Excel.Range xlRange1MLS, Excel.Range xlRange2MLS,
+            Excel.Range xlRange3MLS, Dictionary<string, int> rangeCount, Dictionary<string, int> relevantCols)
         {
             Console.WriteLine("Reached processorWork() function!");
+
+            xlRange1MLS.Cells[2, relevantCols["MLSSellAgentCol1"]].Value = relevantCols["MLSSellAgentCol1"];
+            xlRange1MLS.Cells[2, relevantCols["MLSSellOfficeCol1"]].Value = relevantCols["MLSSellOfficeCol1"];
+            xlRange1MLS.Cells[2, relevantCols["MLSListAgentCol1"]].Value = relevantCols["MLSListAgentCol1"];
+            xlRange1MLS.Cells[2, relevantCols["MLSListOfficeCol1"]].Value = relevantCols["MLSListOfficeCol1"];
+            xlRange1MLS.Cells[2, relevantCols["MLSOwnerCol1"]].Value = relevantCols["MLSOwnerCol1"];
+            xlRange1MLS.Cells[2, relevantCols["MLSCityCol1"]].Value = relevantCols["MLSCityCol1"];
+            xlRange1MLS.Cells[2, relevantCols["MLSAddressCol1"]].Value = relevantCols["MLSAddressCol1"];
+            xlRange1MLS.Cells[2, relevantCols["MLSCloseDateCol1"]].Value = relevantCols["MLSCloseDateCol1"];
+            xlRange1MLS.Cells[2, relevantCols["MLSPriceCol1"]].Value = relevantCols["MLSPriceCol1"];
+            xlRange1MLS.Cells[2, relevantCols["MLSGFCol1"]].Value = relevantCols["MLSGFCol1"];
+            xlRange1MLS.Cells[2, relevantCols["MLSEscrowCol1"]].Value = relevantCols["MLSEscrowCol1"];
+
+            xlRange2MLS.Cells[2, relevantCols["MLSAgentCol2"]].Value = relevantCols["MLSAgentCol2"];
+            xlRange2MLS.Cells[2, relevantCols["MLSOfficeCol2"]].Value = relevantCols["MLSOfficeCol2"];
+            xlRange2MLS.Cells[2, relevantCols["MLSOtherAgentCol2"]].Value = relevantCols["MLSOtherAgentCol2"];
+            xlRange2MLS.Cells[2, relevantCols["MLSOtherOfficeCol2"]].Value = relevantCols["MLSOtherOfficeCol2"];
+            xlRange2MLS.Cells[2, relevantCols["MLSOwnerCol2"]].Value = relevantCols["MLSOwnerCol2"];
+            xlRange2MLS.Cells[2, relevantCols["MLSCityCol2"]].Value = relevantCols["MLSCityCol2"];
+            xlRange2MLS.Cells[2, relevantCols["MLSAddressCol2"]].Value = relevantCols["MLSAddressCol2"];
+            xlRange2MLS.Cells[2, relevantCols["MLSCloseDateCol2"]].Value = relevantCols["MLSCloseDateCol2"];
+            xlRange2MLS.Cells[2, relevantCols["MLSPriceCol2"]].Value = relevantCols["MLSPriceCol2"];
+            xlRange2MLS.Cells[2, relevantCols["MLSGFCol2"]].Value = relevantCols["MLSGFCol2"];
+            xlRange2MLS.Cells[2, relevantCols["MLSEscrowCol2"]].Value = relevantCols["MLSEscrowCol2"];
+            xlRange2MLS.Cells[2, relevantCols["MLSAsSACol2"]].Value = relevantCols["MLSAsSACol2"];
+            xlRange2MLS.Cells[2, relevantCols["MLSClosingsCol2"]].Value = relevantCols["MLSClosingsCol2"];
+            xlRange2MLS.Cells[2, relevantCols["MLSTCCloseCol2"]].Value = relevantCols["MLSTCCloseCol2"];
+            xlRange2MLS.Cells[2, relevantCols["MLSBSClosingCol2"]].Value = relevantCols["MLSBSClosingCol2"];
+            xlRange2MLS.Cells[2, relevantCols["MLSBSTCCloseCol2"]].Value = relevantCols["MLSBSTCCloseCol2"];
+
+            xlRange3MLS.Cells[2, relevantCols["MLSAgentCol3"]].Value = relevantCols["MLSAgentCol3"];
+            xlRange3MLS.Cells[2, relevantCols["MLSOfficeCol3"]].Value = relevantCols["MLSOfficeCol3"];
+            xlRange3MLS.Cells[2, relevantCols["MLSOwnerCol3"]].Value = relevantCols["MLSOwnerCol3"];
+            xlRange3MLS.Cells[2, relevantCols["MLSCityCol3"]].Value = relevantCols["MLSCityCol3"];
+            xlRange3MLS.Cells[2, relevantCols["MLSAddressCol3"]].Value = relevantCols["MLSAddressCol3"];
+            xlRange3MLS.Cells[2, relevantCols["MLSCloseDateCol3"]].Value = relevantCols["MLSCloseDateCol3"];
+            xlRange3MLS.Cells[2, relevantCols["MLSPriceCol3"]].Value = relevantCols["MLSPriceCol3"];
+            xlRange3MLS.Cells[2, relevantCols["MLSGFCol3"]].Value = relevantCols["MLSGFCol3"];
+            xlRange3MLS.Cells[2, relevantCols["MLSEscrowCol3"]].Value = relevantCols["MLSEscrowCol3"];
+            xlRange3MLS.Cells[2, relevantCols["MLSAsSACol3"]].Value = relevantCols["MLSAsSACol3"];
+            xlRange3MLS.Cells[2, relevantCols["MLSClosingsCol3"]].Value = relevantCols["MLSClosingsCol3"];
+            xlRange3MLS.Cells[2, relevantCols["MLSTCCloseCol3"]].Value = relevantCols["MLSTCCloseCol3"];
+            xlRange3MLS.Cells[2, relevantCols["MLSBSClosingCol3"]].Value = relevantCols["MLSBSClosingCol3"];
+            xlRange3MLS.Cells[2, relevantCols["MLSBSTCCloseCol3"]].Value = relevantCols["MLSBSTCCloseCol3"];
         }
     }
 }
